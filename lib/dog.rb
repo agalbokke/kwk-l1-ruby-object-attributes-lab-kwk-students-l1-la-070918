96731dd945add438c3e1d2fc9e7039064a78b10d
Ashyah Hewage <ashyahgalbokke@gmail.com>
2018-07-12 22:26:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,27 +4,14 @@ class Dog
   def initialize = (name, breed)
     @name = name
     @breed = breed
-    
   end
 
 def name 
   @name = name
-  
 end
 
 def name = (new_name)
   @name = new_name
-  
-
 end
 
-def breed
-  @name = breed
-  
-end
 
-def breed = (new_breed)
-  @breed = new_breed
-  
-end
-end
